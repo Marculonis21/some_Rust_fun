b2d98d445dafd881f61a96e9fb017225bd9e7ecc
Marculonis21 <markbecvar@gmail.com>
2025-01-01 15:09:58 +0100

    disjoint set

--- assignments/disjoint_set/src/main.rs
+++ assignments/disjoint_set/src/main.rs
@@ -1,59 +1,54 @@
 use std::io;
 
 struct UnionFind {
-    parent: Vec<i32>,
-    size: Vec<i32>,
-    elements: i32
+    parent: Vec<usize>,
+    size: Vec<usize>,
 }
 
 impl UnionFind {
     fn new(count: usize) -> Self { 
-        let mut parent: Vec<i32> = vec![];
-        let mut size: Vec<i32> = vec![];
+        let mut parent: Vec<usize> = vec![];
+        let mut size: Vec<usize> = vec![];
         parent.resize(count,0);
         size.resize(count,0);
 
         for i in 0..count {
-            parent[i] = i as i32;
+            parent[i] = i ;
             size[i] = 1;
         }
 
         return Self { parent: parent,
                size: size,
-               elements: count as i32
         }
     }
 
-    fn find(&mut self, x: i32) -> i32 {
+    fn find(&mut self, x: usize) -> usize {
         let mut root = x;
 
-        while self.parent[root as usize] != root {
-            self.parent[root as usize] = self.parent[self.parent[root as usize] as usize];
-            root = self.parent[root as usize];
+        while self.parent[root] != root {
+            self.parent[root] = self.parent[self.parent[root]];
+            root = self.parent[root];
         }
 
         return root
     }
 
-    fn merge(&mut self, x: i32, y: i32) {
+    fn merge(&mut self, x: usize, y: usize) {
         let rx = self.find(x);
         let ry = self.find(y);
 
-        if self.size[ry as usize] > self.size[rx as usize] {
-            self.parent[rx as usize] = ry;
-            self.size[ry as usize] += self.size[rx as usize];
+        if self.size[ry] > self.size[rx] {
+            self.parent[rx] = ry;
+            self.size[ry] += self.size[rx];
         }
         else {
-            self.parent[ry as usize] = rx;
-            self.size[rx as usize] += self.size[ry as usize];
+            self.parent[ry] = rx;
+            self.size[rx] += self.size[ry];
         }
-
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-
     let stdin = io::stdin();
 
     let mut uf = UnionFind::new(0);
@@ -63,19 +58,19 @@ fn main() {
         let parts:Vec<&str> = line.split_whitespace().collect();
         match parts[..] {
             ["init", n] => {
-                println!(">> init {n}");
+                // println!(">> init {n}");
                 uf = UnionFind::new(n.parse().unwrap());
             }
 
             ["merge", a, b] => {
-                println!(">> merge {a} {b}");
+                // println!(">> merge {a} {b}");
                 uf.merge(a.parse().unwrap(),
                          b.parse().unwrap());   
             }
             ["find", x] => {
-                println!(">> find {x}");
+                // println!(">> find {x}");
                 let out = uf.find(x.parse().unwrap());
-                println!("OUT: {out}");
+                println!("{out}");
             }
             _ => panic!("Unknown input!")
         }
